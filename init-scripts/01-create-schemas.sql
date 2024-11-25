7773eb0b7b3905f5c044f7eef77df2eee25b7d68
Yuri Claro <yuri.claro@outlook.com>
2024-11-25 10:44:01 -0300

    portal_api: initial scripts for Docker

---
+++ init-scripts/01-create-schemas.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA IF NOT EXISTS schema_authentication AUTHORIZATION postgres;
+CREATE SCHEMA IF NOT EXISTS schema_email AUTHORIZATION postgres;
+CREATE SCHEMA IF NOT EXISTS schema_salary AUTHORIZATION postgres;
