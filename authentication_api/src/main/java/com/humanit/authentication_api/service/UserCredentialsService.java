f2c676a8e5ff2fdf0e0c35f3eabb87a50a374eed
Yuri Claro <yuri.claro@outlook.com>
2024-11-25 10:32:55 -0300

    authentication_api: UserCredentialsService and RefreshTokenService Interfaces.

---
+++ authentication_api/src/main/java/com/humanit/authentication_api/service/UserCredentialsService.java
@@ -0,0 +1,12 @@
+package com.humanit.authentication_api.service;
+
+import com.humanit.authentication_api.dto.AuthRequestDTO;
+import com.humanit.authentication_api.dto.AuthResponseDTO;
+import com.humanit.authentication_api.dto.RefreshTokenRequestDTO;
+import com.humanit.authentication_api.exception.refreshToken.RefreshTokenNotFoundException;
+import com.humanit.authentication_api.exception.userCredentials.UserCredentialsNotAuthenticatedException;
+
+public interface UserCredentialsService {
+    AuthResponseDTO signIn(AuthRequestDTO authRequestDTO) throws UserCredentialsNotAuthenticatedException;
+    AuthResponseDTO refreshToken(RefreshTokenRequestDTO refreshRequestDTO) throws RefreshTokenNotFoundException;
+}
