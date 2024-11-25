94242b11deb37a8872095b3c7063064eac8949bc
Yuri Claro <yuri.claro@outlook.com>
2024-11-25 10:20:51 -0300

    salary_api: Auditable entities

---
+++ salary_api/src/main/java/com/humanit/salary_api/audit/Auditable.java
@@ -0,0 +1,39 @@
+package com.humanit.salary_api.audit;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable implements Serializable {
+    @CreatedBy
+    @NotEmpty
+    private String createdBy;
+
+    @CreatedDate
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @NotEmpty
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @UpdateTimestamp
+    private LocalDateTime lastModifiedAt;
+}
